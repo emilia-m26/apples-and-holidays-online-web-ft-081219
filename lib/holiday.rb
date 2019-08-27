94df0e232d57672bc1afb01218ca6a39463edaa6
Emilia ☆ <emilia.mena26@gmail.com>
2019-08-27 22:00:17 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
     holiday_hash.each do |season, holiday|
       puts "#{season.to_s.capitalize}:"
     holiday.each do |holidays, supplies|
-      puts " #{holidays.to_s.split('_').map {|x| x.capitalize}.join(' ')}: #{supplies.join(", ")}"
+      puts "  #{holidays.to_s.split('_').map {|x| x.capitalize}.join(' ')}: #{supplies.join(", ")}"
     end
   end
 end
